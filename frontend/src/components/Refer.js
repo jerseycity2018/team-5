42856a147764d65c28db129640d6a266e9aafcdf
shadow12ac <42756965+shadow12ac@users.noreply.github.com>
2018-10-27 07:44:58 -0500

    Added Refer.js

---
+++ frontend/src/components/Refer.js
@@ -0,0 +1,9 @@
+<div>
+	<h1><u>Recomandation</u></h1>
+		<p>Refer this app and both your friend and you will earn 10 points</p>
+		<input type="text" name="Phonenumber" value="Phone Number">
+
+
+
+
+</div>
