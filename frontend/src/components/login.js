4770722043caafa4ec643057d01a1578031ee8fa
Chris Panican <chrispanican@gmail.com>
2018-10-27 01:27:26 -0400

    Finish Login Page

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
-import { Row, Input } from 'react-materialize';
+import { Row, Input, Container, Button } from 'react-materialize';
+import { Link } from 'react-router-dom';
 import '../styles/login.css';
 
 class Login extends Component {
     render() {
         return (
-            <div>
-                <h1>Login Page</h1>
+            <div className="login">
+                <h1 className="header-spacing">Log In</h1>
+                <Container>
+                    <Row>
+                        <Input placeholder="What is your user name?" s={12} label="User name" />
+                        <Input type="password" placeholder="What is your password?" s={12} label="Password" />
+                    </Row>
+                </Container>
+                <Link to={{ pathname:'/home' }}>
+                    <Button className="button-style">Log In</Button>
+                </Link>
+                <Link to={{ pathname:'/register' }}>
+                    <Button className="button-style">Sign Up</Button>
+                </Link>
             </div>
         );
     }
