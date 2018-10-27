3be9d6e74cf84ec124985bfc0038f7a3c7055888
mousseinov <moussei000@citymail.cuny.edu>
2018-10-27 00:09:45 -0400

    top communities

--- backend/endpoint.py
+++ backend/endpoint.py
@@ -47,8 +47,7 @@ def topTenInCommunity(zipcode):
 	for row in reader:
 		data.append(row)
 	for row in data:
-		if(row[0] == "User_ID"):
-			pass
+		if row[0] == "User_ID": pass
 		else:
 			print(row[4])
 			if row[4] == zipcode:
@@ -61,6 +60,40 @@ def topTenInCommunity(zipcode):
 		result["users"].append((pq.get())[1])
 	
 	return json.dumps(result)
+
+@app.route("/leaderboard/topcommunity/", methods = ['GET'])
+def topTenCommunities():
+	communityMap = {}
+	pq = PriorityQueue()
+	reader = csv.reader(open('data.csv', 'r'))
+	data = []
+	count = 0
+	for row in reader:
+		if row[4] not in communityMap:
+			communityMap[row[4]] = int(row[3])
+			count += 1
+		else:
+			communityMap[row[4]] += int(row[3])
+
+	for community in communityMap:
+		pq.put((-communityMap[community], community))
+
+	result = {"communities":[]}
+	if count > 10: count = 10
+	for i in range(count):
+		curr = pq.get()
+		currRes = {"community": curr[1], "bottles": -curr[0]}
+		result["community"].append(currRes)
+
+	return json.dumps(result)
+
+
+
+
+
+
+
+		
 
 
 
