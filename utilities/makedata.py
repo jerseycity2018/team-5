52c7725465ffd4ebc536ff91a6e1b9b32c8a3921
Mayank Vanjani <MayankVanjani@Mayanks-MacBook-Pro.local>
2018-10-26 22:51:16 -0400

    Better Naming

--- utilities/makedata.py
+++ utilities/makedata.py
@@ -1,16 +1,26 @@
 import csv
 import random
 
-with open('newdata.csv', mode='w') as newdata:
+with open('data.csv', mode='w') as data:
     
-    data_write = csv.writer(newdata, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    data_write = csv.writer(data, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
     data_write.writerow([ 'User_ID', 'Name', 'Username', 'Total_Bottles', 'Community' ])
-    Nums = random.sample(range(1,1000), 500)
-    
-    for i in range(500):
-        data_write.writerow([ str( Nums[i] ), "Test_Name " + str(i),
-                              "Username" + str(i), str((i+1)*Nums[i]%100), str(10000 + (i*3)) ]);
+    nums = random.sample(range(1,1000), 100)
+
+    namefile = open("all_names.txt","r")
+    names = []
+    for line in namefile:
+        names.append(line.split(None,1)[0])
+
+    #print(len(names))
+    #print(type(names[0]))
+    name_counter = 0;
+    for i in range(100):
+        data_write.writerow([ str( nums[i] ), names[name_counter]+" "+names[name_counter+1],
+                              "@"+names[name_counter]+str(i*2), str((i+1)*nums[i]%100),
+                              str(10000 + (i*3)) ]);
+        name_counter += 2
         
     print()
     
