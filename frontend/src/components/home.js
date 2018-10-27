735887fbb8617daeac1b269efc5016a4cbdcc7b8
Chris Panican <chrispanican@gmail.com>
2018-10-26 21:11:42 -0400

    Initial frontend files

---
+++ frontend/src/components/home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/home.css';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Home Page</h1>
+            </div>
+        );
+    }
+}
+
+export default Home;
---
+++ frontend/src/components/myFooter.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/myFooter.css';
+
+class MyFooter extends Component {
+    render() {
+        return (
+            <div>
+                <footer>&copy; Team 5 Guys</footer>
+            </div>
+        )
+    }
+}
+
+export default MyFooter;
