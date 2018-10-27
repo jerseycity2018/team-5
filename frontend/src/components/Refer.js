d9d52f4d9d64f4671aec96f8d0dcd22ae7262a7f
Chris Panican <chrispanican@gmail.com>
2018-10-27 08:48:52 -0400

    Add Refer and Rewards

--- frontend/src/components/Refer.js
+++ frontend/src/components/Refer.js
@@ -1,9 +1,18 @@
-<div>
-	<h1><u>Recomandation</u></h1>
-		<p>Refer this app and both your friend and you will earn 10 points</p>
-		<input type="text" name="Phonenumber" value="Phone Number">
+import React, { Component } from 'react';
+import { Row, Input, Container, Button } from 'react-materialize';
+import { Link } from 'react-router-dom';
+import '../styles/refer.css';
 
+class Refer extends Component {
+    render() {
+        return (
+			<div>
+				<h1><u>Recomandation</u></h1>
+				<p>Refer this app and both your friend and you will earn 10 points</p>
+				<input type="text" name="Phonenumber" value="Phone Number" />
+			</div>
+        );
+    }
+}
 
-
-
-</div>
+export default Refer;
