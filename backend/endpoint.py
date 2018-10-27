637c019799b58fc292b59fe94a7b0e10ec866730
mousseinov <moussei000@citymail.cuny.edu>
2018-10-26 20:22:51 -0400

    top users started and data.txt added

--- backend/endpoint.py
+++ backend/endpoint.py
@@ -1,11 +1,26 @@
 from flask import Flask
 from uszipcode import SearchEngine
+import json
 
 app = Flask(__name__)
 
 
+@app.route("/", methods = ['GET'])
+def index():
+	return '<h1>Welcome to Code for good API: Rare Project!</h1>'
+
 @app.route("/zipcode/<zipcode>", methods = ['GET'])
-def hello(zipcode):
+def zip(zipcode):
 	search = SearchEngine(simple_zipcode=True)
 	result = search.by_zipcode(zipcode)
 	return result.to_json()
+
+
+@app.route("/leaderboard/<type>", methods = ['GET'])
+def topTenUsers(type):
+	if type == "users":
+		topTenUsers = {"users": [{'name': 'bob', 'bottle': 100, 'community': '11444'}] }
+		return json.dumps(topTenUsers)
+
+	elif type == "community":
+		pass
